2768d07b3ac69ebff01f5ebc96c5e69dbf3317e0
seojoo95 <120086588+seojoo95@users.noreply.github.com>
2024-07-27 15:40:54 +0900

    [워들] Javascript 로직 완선
    워들 게임을 만들어 봤습니다

---
+++ js/index.js
@@ -0,0 +1,103 @@
+const 정답 = "APPLE";
+
+let attempts = 0;
+let index = 0;
+let timer;
+
+function appStart() {
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position:absolute; left: 40vw; top:40vh; background-color: #fff; width: 200px; height: 100px;";
+    document.body.appendChild(div);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts++;
+    index = 0;
+  };
+  const gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+  const handleEnterKey = () => {
+    let 맞은_갯수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const 입력한_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+
+      if (입력한_글자 === 정답_글자) {
+        맞은_갯수++;
+        block.style.backgroundColor = "#6aaa64";
+        block.style.color = "#ffffff";
+      } else if (정답.includes(입력한_글자)) {
+        block.style.backgroundColor = "#c9b458";
+      } else {
+        block.style.backgroundColor = "#787c7e";
+        block.style.color = "#ffffff";
+      }
+    }
+    if (맞은_갯수 === 5) {
+      gameover();
+    } else {
+      nextLine();
+    }
+  };
+
+  const hanbleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) {
+      index -= 1;
+    }
+  };
+
+  const handleKeydown = (event) => {
+    const key = event.key.toUpperCase();
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+    if (event.key === "Backspace") hanbleBackspace();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (event.key === "Enter") handleEnterKey();
+    else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index++;
+    }
+  };
+
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 시간 = 흐른_시간.getHours().toString();
+      const 분 = 흐른_시간.getMinutes().toString();
+      const 초 = 흐른_시간.getSeconds().toString();
+      const timeDiv = document.querySelector(".time");
+      timeDiv.innerText = `${분.padStart(2, "0")}:${초.padStart(2, "0")}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+  startTimer();
+  window.addEventListener("keydown", handleKeydown);
+}
+
+appStart();
+//키보드에도 동일하게 표기
+//키보드 클릭에도 표기
